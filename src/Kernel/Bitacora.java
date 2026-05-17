62ef0afa1afa7eb370dce45e9667caa26686e102
GARAVITO HORMAZA JOHAN CAMILO <jcgaravitoh@udistrital.edu.co>
2026-05-17 11:28:00 -0500

    add Bitacora constructor getters and setters

--- src/Kernel/Bitacora.java
+++ src/Kernel/Bitacora.java
@@ -1,4 +1,35 @@
 package Kernel;
 
 public class Bitacora {
-}
+
+    // Atributos
+    private int id;
+    private String accion;
+    private String detalle;
+    private String fecha;
+
+    // Constructor
+    public Bitacora(int id, String accion, String detalle, String fecha) {
+        // Datos del registro.
+        this.id = id;
+        this.accion = accion;
+        this.detalle = detalle;
+        this.fecha = fecha;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getAccion() {
+        return accion;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
