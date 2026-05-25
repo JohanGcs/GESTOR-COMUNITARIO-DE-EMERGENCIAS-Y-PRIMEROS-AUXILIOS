ac0827727f823ea712dba080e6952c5663b5f255
Jose_M <joseluismelovelandia@gmail.com>
2026-05-24 21:49:16 -0500

    Create Reporte.java

---
+++ src/Kernel/Reporte.java
@@ -0,0 +1,22 @@
+package modelo;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Reporte {
+
+    /**
+     * Cuenta incidentes por tipo
+     */
+    public void generarPorTipo(List<Incidente> lista, String tipo) {
+
+        int contador = 0;
+
+        for (Iterator<Incidente> iterator = lista.iterator(); iterator.hasNext();) {
+			// Que recorra incidentes y asigne
+            contador++;
+		}
+
+        System.out.println("Total de incidentes tipo " + tipo + ": " + contador);
+    }
+}
