300832cedcece8059be8364e77ec2e5ca914789f
Jose_M <joseluismelovelandia@gmail.com>
2026-05-24 21:51:38 -0500

    Create TipoMovimiento.java

---
+++ src/Kernel/TipoMovimiento.java
@@ -0,0 +1,24 @@
+package modelo;
+
+/**
+ * Clase con tipos de movimiento
+ * del inventario.
+ *
+ * Se usa para:
+ * - entradas
+ * - salidas
+ *
+ * Evita escribir texto distinto
+ * en muchas partes del programa.
+ */
+public class TipoMovimiento {
+
+
+    // CONSTANTES
+
+    public static final String ENTRADA =
+        "ENTRADA";
+
+    public static final String SALIDA =
+        "SALIDA";
+}
