b465df8da33ebf742c5f097a9e894f3fef12250a
Jose_M <joseluismelovelandia@gmail.com>
2026-05-24 21:41:23 -0500

    Create Coordinador.java

---
+++ src/Kernel/Coordinador.java
@@ -0,0 +1,37 @@
+package modelo;
+
+
+import java.util.List;
+
+public class Coordinador extends Usuario { //Coordinador parte de usuario
+
+    public Coordinador(String id, String nombre) {
+        super(id, nombre);
+    }
+
+    @Override
+    public void accion() { //Multiples acciones
+        System.out.println(getNombre() + " coordina el sistema");
+    }
+
+    /**
+     * Asignar brigadista
+     */
+    public void asignar(Incidente i, Brigadista b) {
+        i.asignarBrigadista(b);
+    }
+
+    /**
+     * Buscar brigadista disponible
+     */
+    public Brigadista buscarDisponible(List<Brigadista> lista) {
+
+        for (Brigadista b : lista) {
+            if (b.isDisponible()) {
+                return b;
+            }
+        }
+
+        return null;
+    }
+}
