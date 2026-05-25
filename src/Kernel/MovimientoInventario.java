637aa6fbc54333cffc852e8c5dbe26226f934ed9
Jose_M <joseluismelovelandia@gmail.com>
2026-05-24 21:48:47 -0500

    Create MovimientoInventario.java

---
+++ src/Kernel/MovimientoInventario.java
@@ -0,0 +1,37 @@
+package modelo;
+
+import java.time.LocalDateTime; //Ajuste en tiempo real
+
+public class MovimientoInventario {
+
+    private String tipo; // "entrada" o "salida"
+    private int cantidad; //Cuantos saca
+    private LocalDateTime fecha;
+    private Suministro suministro;
+
+    public MovimientoInventario(String tipo, int cantidad, Suministro suministro) { //Los cambios de suministro
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+        this.suministro = suministro;
+        this.setFecha(LocalDateTime.now());
+    }
+
+    public void aplicar() {
+
+        if (tipo.equalsIgnoreCase("entrada")) {
+            suministro.agregarStock(cantidad);
+        } else if (tipo.equalsIgnoreCase("salida")) {
+            suministro.usarStock(cantidad);
+        } else {
+            System.out.println("Tipo inválido");
+        }
+    }
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+}
