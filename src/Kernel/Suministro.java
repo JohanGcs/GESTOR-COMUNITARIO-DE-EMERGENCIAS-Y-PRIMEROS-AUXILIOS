cf58959a3169f61c667523927b9d0b766fe7f01d
GARAVITO HORMAZA JOHAN CAMILO <jcgaravitoh@udistrital.edu.co>
2026-05-31 00:24:07 -0500

    Edit Suministro Class

--- src/Kernel/Suministro.java
+++ src/Kernel/Suministro.java
@@ -36,11 +36,31 @@ public class Suministro { //Constructor
 
     public String getNombre() { return nombre; }
 
-	public String getId() {
-		return id;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getStockMinimo() {
+        return stockMinimo;
+    }
+
+    public void setStockMinimo(int stockMinimo) {
+        this.stockMinimo = stockMinimo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
